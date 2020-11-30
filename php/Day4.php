047630c11ff2b2c33ad882cccb7618680cf9d59f
Josh Gaber <j.gaber@vehikl.com>
2020-11-29 19:34:56 -0500

    Day 4 & 5

--- php/Day4.php
+++ php/Day4.php
@@ -4,12 +4,26 @@ namespace AdventOfCode;
 
 class Day4
 {
+    private const HASH = 'iwrupvqb';
+
     public function part1 ()
     {
+        $i = 0;
+        do {
+            $hash = md5(self::HASH . ++$i);
+        } while (substr($hash, 0, 5) !== '00000');
+
+        printf("Lowest integer: %d\n", $i);
     }
 
     public function part2 ()
     {
+        $i = 0;
+        do {
+            $hash = md5(self::HASH . ++$i);
+        } while (substr($hash, 0, 6) !== '000000');
+
+        printf("Lowest integer: %d\n", $i);
     }
 }
 
--- php/Day5.php
+++ php/Day5.php
@@ -4,12 +4,34 @@ namespace AdventOfCode;
 
 class Day5
 {
+    private array $strings;
+
+    public function __construct()
+    {
+        $this->strings = explode("\n", trim(load('day5.txt')));
+    }
+
     public function part1 ()
     {
+        $nice = array_filter(
+            $this->strings,
+            fn ($s) => preg_match("/(.)\\1/", $s)
+                && preg_match("/.*[aeiou].*[aeiou].*[aeiou].*/", $s)
+                && !preg_match("/ab|cd|pq|xy/", $s)
+        );
+
+        printf("Nice strings: %d\n", count($nice));
     }
 
     public function part2 ()
     {
+        $nice = array_filter(
+            $this->strings,
+            fn ($s) => preg_match("/(..).*\\1/", $s)
+                && preg_match("/(.).\\1/", $s)
+        );
+
+        printf("Nice strings: %d\n", count($nice));
     }
 }
 
