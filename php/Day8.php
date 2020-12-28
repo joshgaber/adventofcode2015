5ea4dffedcfd17600c558359c91128049f7f9a73
Josh Gaber <j.gaber@vehikl.com>
2020-12-27 23:36:54 -0500

    Day 8 Complete

--- php/Day8.php
+++ php/Day8.php
@@ -4,12 +4,34 @@ namespace AdventOfCode;
 
 class Day8
 {
-    public function part1 ()
+    public function __construct()
     {
+        $this->strings = explode("\n", load('day8.txt'));
     }
 
-    public function part2 ()
+    public function part1()
     {
+        $diff = array_sum(array_map(fn($item) => $this->unescapeDiff($item), $this->strings));
+        printf("Space saved after unescaping: %d\n", $diff);
+    }
+
+    public function part2()
+    {
+        $diff = array_sum(array_map(fn($item) => $this->escapeDiff($item), $this->strings));
+        printf("Space used after escaping: %d\n", $diff);
+
+    }
+
+    private function unescapeDiff($string)
+    {
+        $escaped = substr(preg_replace(['/\\\\\\\\/', '/\\\\"/', '/\\\\x\w\w/'], ['.',',','?'], $string), 1, -1);
+        return strlen($string) - strlen($escaped);
+    }
+
+    private function escapeDiff($string)
+    {
+        $unescape = '"'.preg_replace(['/\\\\/', '/"/'], ['\\\\\\\\','\\"'], $string).'"';
+        return strlen($unescape) - strlen($string);
     }
 }
 
