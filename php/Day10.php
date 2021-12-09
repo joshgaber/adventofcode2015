706632f1ddfe5a36b45437e104018825f5fe9f7e
Josh Gaber <j.gaber@vehikl.com>
2021-12-08 22:16:03 -0500

    Day 10 Complete

--- php/Day10.php
+++ php/Day10.php
@@ -4,12 +4,56 @@ namespace AdventOfCode;
 
 class Day10
 {
+    private string $sequence;
+
+    public function __construct() {
+        $this->sequence = trim(load('day10.txt'));
+    }
+
     public function part1 ()
     {
+        $sequence = $this->sequence;
+
+        for($i = 0; $i < 40; $i++) {
+            $sequence = $this->resequence($sequence);
+        }
+
+        echo "PART 1: " . strlen($sequence) . "\n";
     }
 
     public function part2 ()
     {
+        $sequence = $this->sequence;
+
+        for($i = 0; $i < 50; $i++) {
+            $sequence = $this->resequence($sequence);
+        }
+
+        echo "PART 2: " . strlen($sequence) . "\n";
+    }
+
+    public function resequence(string $sequence): string
+    {
+        $newSequence = [];
+        $sequenceArray = str_split($sequence);
+        $prevChar = array_shift($sequenceArray);
+        $count = 1;
+
+        foreach ($sequenceArray as $char) {
+            if ($char == $prevChar) {
+                $count++;
+            } else {
+                $newSequence[] = "{$count}";
+                $newSequence[] = $prevChar;
+                $prevChar = $char;
+                $count = 1;
+            }
+        }
+
+        $newSequence[] = "{$count}";
+        $newSequence[] = $prevChar;
+
+        return implode('', $newSequence);
     }
 }
 
