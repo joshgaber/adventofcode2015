bb17f67ab9eaf264320ebd24458ab5ab21ca95db
Josh Gaber <j.gaber@vehikl.com>
2021-12-12 15:36:49 -0500

    Day 12 Complete

--- php/Day12.php
+++ php/Day12.php
@@ -4,12 +4,43 @@ namespace AdventOfCode;
 
 class Day12
 {
-    public function part1 ()
+    public function __construct(private readonly string $books) {}
+
+    public function part1()
     {
+        $matches = [];
+        preg_match_all('/-?\\d+/', $this->books, $matches);
+
+        $total = array_sum(array_map(fn ($num) => intval($num), $matches[0]));
+        echo "PART 1: {$total}\n";
     }
 
-    public function part2 ()
+    public function part2()
     {
+        $books = json_decode($this->books, true);
+        $books = $this->array_remove_red($books);
+        $fixedBooks = json_encode($books);
+
+        $matches = [];
+        preg_match_all('/-?\\d+/', $fixedBooks, $matches);
+
+        $total = array_sum(array_map(fn ($num) => intval($num), $matches[0]));
+        echo "PART 1: {$total}\n";
+    }
+
+    private function array_remove_red(array $array): array
+    {
+        foreach($array as $key => &$value) {
+            if (is_array($value)) {
+                if (!array_is_list($value) && in_array('red', $value)) {
+                    unset($array[$key]);
+                } else {
+                    $array[$key] = $this->array_remove_red($value);
+                }
+            }
+        }
+
+        return array_values($array);
     }
 }
 
